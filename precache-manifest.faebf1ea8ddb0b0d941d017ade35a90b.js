fe4b9f2daf97718b48d6a276e0c4a810933b4c33
Jheison Talavera <jheison.talavera@gmail.com>
2024-01-15 15:58:54 -0500

    Updates

---
+++ precache-manifest.faebf1ea8ddb0b0d941d017ade35a90b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "799229e78c178d3ab029c763100b82e8",
+    "url": "/curso-react-intro/index.html"
+  },
+  {
+    "revision": "5b17e8a28344e3aa9aef",
+    "url": "/curso-react-intro/static/css/main.076ce44c.chunk.css"
+  },
+  {
+    "revision": "95d04c91666b236f485a",
+    "url": "/curso-react-intro/static/js/2.8696a317.chunk.js"
+  },
+  {
+    "revision": "5b17e8a28344e3aa9aef",
+    "url": "/curso-react-intro/static/js/main.1e8be718.chunk.js"
+  },
+  {
+    "revision": "a3e35e612aa97b643afb",
+    "url": "/curso-react-intro/static/js/runtime~main.0dde7f30.js"
+  },
+  {
+    "revision": "9cd0f7fbff97716cdf38de057b74d6c0",
+    "url": "/curso-react-intro/static/media/check.9cd0f7fb.svg"
+  },
+  {
+    "revision": "9e128a7861a1b8f8f793d9a635f57029",
+    "url": "/curso-react-intro/static/media/delete.9e128a78.svg"
+  }
+]);
